1ab05a5d20024098b01cc4cc8509a0bf98c05c1f
Ignacio <ignacio.irigoitia@gmail.com>
2024-10-11 20:39:03 -0300

    linter

--- app/dashboard/supplie/page.tsx
+++ app/dashboard/supplie/page.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
-import { X, Upload } from 'lucide-react'
+import { Upload } from 'lucide-react'
 
 type Invoice = {
   id: number
